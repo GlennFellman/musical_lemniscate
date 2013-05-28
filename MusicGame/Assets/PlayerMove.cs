4bb0767c892e9f4c1d1777bd8c5b9ea9cff664a0
fparedesg <fparedesgarza@gmail.com>
2013-05-28 18:33:18 -0500

    Fixed Scene

---
+++ MusicGame/Assets/PlayerMove.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+	public bool isOnTop;
+	public int level;
+	
+	private const float BOTTOMLAYER = -7.120502f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
